c37d7fabe5e83bf27e44882a564e6843752df0e2
VESTAS\SISHE <sishe@vestas.com>
2025-03-11 19:22:21 +0100

    back to business agaian

--- generate-cert.sh
+++
@@ -1,5 +0,0 @@
-openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/private/key.pem -out /etc/ssl/certs/cert.pem -days 365 -nodes \
-    -subj "/C=US/ST=State/L=City/O=Company/OU=Org/CN=localhost"
-
-# Start Nginx
-nginx -g "daemon off;"
